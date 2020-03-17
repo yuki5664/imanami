2f8c729ccbe1f3a9ad192b1d9080259602591050
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-17 16:45:35 +0900

    ad 一覧ページ edit機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
   def new
     @post = Post.new # フォーム用の空のインスタンスを生成する。
   end
@@ -15,6 +20,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿を更新しました。"
+    else
+      render :edit
+    end
+  end
+
 
   private
 
