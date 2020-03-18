d00d2d3a7a4fb6803d1647f4c6467077e0249e96
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-18 11:07:09 +0900

    add log_in機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
   def index
     @posts = Post.order(created_at: :desc)
   end
@@ -18,15 +20,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "投稿を更新しました。"
     else
@@ -35,7 +34,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました。"
   end
@@ -46,5 +44,10 @@ class PostsController < ApplicationController
   def post_params # ストロングパラメータを定義する
     params.require(:post).permit(:area, :point, :caption, :image)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
 
