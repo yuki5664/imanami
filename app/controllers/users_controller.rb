5be1090da191f1df8ef38b8c28b7bdf165372ffd
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-19 21:42:53 +0900

    imanami_logo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def show
+    
+    @user = User.find(params[:id])
+    @posts = Post.with_attached_image.where(user_id: @user).order(created_at: :desc).page(params[:page]).per(1)
+  end
+end
