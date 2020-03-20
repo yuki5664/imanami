55ba57364cd3a7ea3e37c5e3f20c9d46798f07ea
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-20 14:06:25 +0900

    add user照会ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  def show
-    
+  def show   
     @user = User.find(params[:id])
-    @posts = Post.with_attached_image.where(user_id: @user).order(created_at: :desc).page(params[:page]).per(1)
+    @posts = Post.with_attached_image.where(user_id: @user).order(created_at: :desc).page(params[:page]).per(12)
   end
 end
