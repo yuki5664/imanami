dba08704e17bb1cdf5c162970893212d63e05476
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-05-18 15:45:27 +0900

    でプローイ

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Imanami
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
+    config.assets.initialize_on_precompile = false
+
   end
 end
